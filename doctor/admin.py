e8f87a491efe2fb3bce04cf77015eb20818519cc
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-03-06 02:34:20 +0600

    init commit

---
+++ doctor/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from .models import Patient, Assistant, Doctor, Appointment
+
+class PatientAdmin(admin.ModelAdmin):
+  fields = ['name', 'age', 'gender']
+
+class AssistantAdmin(admin.ModelAdmin):
+  fields = ['name', 'docid']
+
+class DoctorAdmin(admin.ModelAdmin):
+  fields = ['name']
+
+class AppointmentAdmin(admin.ModelAdmin):
+  fields = ['confirmed', 'patid', 'docid', 'assid']
+
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Assistant, AssistantAdmin)
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Appointment, AppointmentAdmin)
---
+++ doctor/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Patient(models.Model):
+  name = models.CharField(max_length=50)
+  age = models.PositiveSmallIntegerField()
+  gender = models.CharField(max_length=1)
+
+  def __str__(self):
+    return self.name
+
+class Doctor(models.Model):
+  name = models.CharField(max_length=50)
+  max_slots = models.PositiveIntegerField(default=10)
+  available = models.PositiveIntegerField(default=0)
+
+  def __str__(self):
+    return self.name
+
+class Assistant(models.Model):
+  name =  models.CharField(max_length=50)
+  docid = models.OneToOneField('Doctor', on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
+
+class Appointment(models.Model):
+  confirmed = models.BooleanField(default=False)
+  patid = models.OneToOneField('Patient', on_delete=models.CASCADE)
+  docid = models.ForeignKey('Doctor', on_delete=models.CASCADE)
+  assid = models.ForeignKey('Assistant', on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.patid
