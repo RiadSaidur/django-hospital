5729d87acda406981649efa3d46430f192dc41f8
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-03-07 01:33:45 +0600

    added: homepage basic structure

--- doctor/views.py
+++ doctor/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from doctor.models import AppointmentForm
 
 def index(request):
+  context = {
+    'form': AppointmentForm()
+  }
   return render(request, 'index.html')
