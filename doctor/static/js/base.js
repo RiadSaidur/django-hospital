82cb59cc984e854cb1452d5a706eb88c28e3e169
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-03-14 02:24:05 +0600

    organized: extended all pages to base

---
+++ doctor/static/js/base.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+
+  const burger = document.querySelector('aside img')
+  const divs = document.querySelectorAll('nav div')
+  const divArray = [...divs]
+
+  burger.addEventListener('click', () => {
+    divArray.forEach(div => div.classList.toggle('menu'))
+  })
+
+  document
+    .querySelector('main')
+    .addEventListener('click', () => {
+      divArray.forEach(div => div.classList.remove('menu'))
+    })
+
+})
