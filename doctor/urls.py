b11a4857e30b578e02e6a128b855adea46eee2b9
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-03-14 02:23:20 +0600

    added: register page

--- doctor/urls.py
+++ doctor/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, register
 
 urlpatterns = [
     path('', index, name='index'),
+    path('register/', register, name='register'),
 ]
--- doctor/views.py
+++ doctor/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render, HttpResponse
-from doctor.models import AppointmentForm
+from django.shortcuts import render, redirect
+from doctor.forms.registerFrom import UserRegistrationForm
+from django.contrib import messages
 
 def index(request):
-  context = {
-    'form': AppointmentForm()
-  }
-  print(request)
   return render(request, 'index.html')
+
+def register(request):
+  if request.method == 'POST':
+    form = UserRegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      user = form.cleaned_data.get('username')
+      print('NICE')
+      messages.success(request, 'NICE BRO!')
+      return redirect('index')
+  else:
+    form = UserRegistrationForm()
+
+  context = {
+    'form': form
+  }
+
+  return render(request, 'register.html', context)
