4fe5748ff1a186a2f6685b9b6bb6a8d0a2af82b1
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-03-22 14:32:29 +0600

    updated: Patient model to hold dob, gender to choice

--- doctor/admin.py
+++ doctor/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Patient, Request, Appointment
 
 class PatientAdmin(admin.ModelAdmin):
-  fields = ['user', 'age', 'gender', 'picture']
+  fields = ['user', 'fullname', 'dob', 'age', 'gender', 'picture']
 
 class RequestAdmin(admin.ModelAdmin):
   fields = ['confirmed', 'patient', 'doctor']
--- doctor/models.py
+++ doctor/models.py
@@ -3,10 +3,17 @@ from django.contrib.auth.models import User
 from assistant.models import Doctor
 
 class Patient(models.Model):
+  GENDER_CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female')
+  )
+
   user = models.OneToOneField(User, on_delete=models.CASCADE)
+  fullname = models.CharField(max_length=50, null=True)
   age = models.PositiveSmallIntegerField(null=True, blank=True)
-  gender = models.CharField(max_length=6, null=True, blank=True)
+  gender = models.CharField(max_length=6, choices=GENDER_CHOICES, null=True, blank=True)
   picture = models.ImageField(null=True)
+  dob = models.DateField(auto_now=False, auto_now_add=False, null=True)
 
   def __str__(self):
     return str(self.user.username)
