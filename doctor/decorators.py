e15b75b7de34d29e32d7af0d315cec25cdaf9988
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-03-22 17:08:43 +0600

    organized: set up Home iew for authenticated user

--- doctor/decorators.py
+++ doctor/decorators.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 def only_unauthenticated(view_func):
   def warpper_func(request, *args, **kwargs):
     if request.user.is_authenticated:
-      return redirect('index')
+      return redirect('home')
     else:
       return view_func(request, *args, **kwargs)
   return warpper_func
--- doctor/forms.py
+++ doctor/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from doctor.models import Request
+from doctor.models import Request, Patient
 
 class UserRegistrationForm(UserCreationForm):
   email = forms.EmailField()
@@ -14,3 +14,8 @@ class RequestForm(forms.ModelForm):
   class Meta:
     model = Request
     fields = ["doctor"]
+
+class PatientForm(forms.ModelForm):
+  class Meta:
+    model = Patient
+    fields = ['fullname', 'dob', 'age', 'gender']
