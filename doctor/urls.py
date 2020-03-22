4ef2f261975966d1204b4d4ad3af62aa134d5ad4
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-03-22 17:08:09 +0600

    added: updating profile route

--- doctor/urls.py
+++ doctor/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import index, signup, makeAppointment, create_profile, profile, history
+from .views import index, signup, makeAppointment, update_profile, profile, history, home
 
 urlpatterns = [
     path('', index, name='index'),
+    path('home/', home, name='home'),
     path('history/<int:page>', history, name='history'),
     path('profile/', profile, name='profile'),
-    path('create_profile/', create_profile, name='create_profile'),
+    path('update_profile/', update_profile, name='update_profile'),
     path('signup/', signup, name='signup'),
     path('signin/', LoginView.as_view(template_name = 'signin.html', redirect_authenticated_user=True), name='signin'),
     path('signout/', LogoutView.as_view(template_name = 'signout.html'), name='signout'),
--- doctor/views.py
+++ doctor/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, redirect
-from doctor.forms import UserRegistrationForm, RequestForm
+from doctor.forms import UserRegistrationForm, RequestForm, PatientForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Request, Appointment, Patient
 from doctor.decorators import only_unauthenticated
+
+@login_required
+def home(request):
+  prev_app = Request.objects.filter(patient__user__username = request.user.username).first()
+  next_app = Appointment.objects.filter(request__patient__user__username = request.user.username).first()
+  context = {
+    'prev_app': prev_app,
+    'next_app': next_app
+  }
+  return render(request, 'home.html', context)
 
 @login_required
 def history(request, page):
@@ -47,28 +57,37 @@ def makeAppointment(request):
   return render(request, 'makeAppointment.html', context)
 
 @login_required
-def create_profile(request):
-  context = {}
-  return render(request, 'create_profile.html', context)
+def update_profile(request):
+  patient = Patient.objects.get(user = request.user)
+  photoURL = patient.picture.url
+  if request.method == 'POST':
+    form = PatientForm(request.POST, instance = patient)
+    
+    if form.is_valid():
+      form.save()
+      messages.success(request, 'Profile updated!')
+    
+  else: 
+    form = PatientForm(instance = patient)
+  context = {
+    'form': form,
+    'photoURL': photoURL
+  }
+  return render(request, 'update_profile.html', context)
 
 @login_required
 def profile(request):
   patient = Patient.objects.get(user__username = request.user.username)
   context = {
-    'fullname': request.user.get_full_name(),
     'email': request.user.email,
     'username': request.user.username,
     'patient': patient
   }
   return render(request, 'profile.html', context)
 
+@only_unauthenticated
 def index(request):
-  prev_app = Request.objects.filter(patient__user__username = request.user.username).first()
-  next_app = Appointment.objects.filter(request__patient__user__username = request.user.username).first()
-  context = {
-    'prev_app': prev_app,
-    'next_app': next_app
-  }
+  context = {}
   return render(request, 'index.html', context)
 
 @only_unauthenticated
