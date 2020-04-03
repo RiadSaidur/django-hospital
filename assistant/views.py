b42f4f7f0db6ff9d508340ebf84a8b9f8caaf4dc
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-04-04 02:54:31 +0600

    added: reqs and accepted reqs view

--- assistant/views.py
+++ assistant/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
+# decorators
 from django.contrib.auth.decorators import login_required
+# models
+from patient.models import Request, Appointment
+# date-time
+from django.utils import timezone
+# forms
+from assistant.forms import RequestApproveForm
 
 @login_required
 def index(request):
-  context = {}
+  reqs = Request.objects.filter(confirmed = False ,created_at__date__gte = timezone.now().date()).order_by('created_at')
+  accepts = Request.objects.filter(confirmed = True ,created_at__date__gte = timezone.now().date()).order_by('created_at')
+
+  # forms = []
+
+  # for req in reqs:
+  #   if request.method == 'POST':
+  #     form = 
+  #   collection = {
+  #     'req': req,
+  #     'form': 
+  #   }
+  #   forms.append(req.doctor)
+
+  # print(forms)
+  
+  context = {
+    'reqs': reqs,
+    'accepts': accepts,
+    'doctor': reqs[0].doctor
+  }
+
   return render(request, 'assistant/index.html', context)
