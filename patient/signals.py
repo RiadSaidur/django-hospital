1d95108acbbe63785e80eb8bbf202aad27268159
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-04-04 02:26:10 +0600

    updated: available slots for Doctor

--- patient/signals.py
+++ patient/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 # models
 from django.contrib.auth.models import User
 from .models import Request, Appointment
-from assistant.models import Assistant
+from assistant.models import Assistant, Doctor
 
 @receiver(post_save, sender=Request)
 def update_notification(sender, instance, created, **kwargs):
@@ -23,3 +23,16 @@ def delete_notification(sender, instance, using, **kwargs):
     assistant = Assistant.objects.get(doctor=instance.doctor)
     assistant.notifications -= 1
     assistant.save()
+
+@receiver(post_save, sender=Appointment)
+def update_slots(sender, instance, created, **kwargs):
+  if created:
+    doctor = Doctor.objects.get(name=instance.request.doctor)
+    doctor.available -= 1
+    doctor.save()
+
+@receiver(post_delete, sender=Appointment)
+def delete_slots(sender, instance, using, **kwargs):
+    doctor = Doctor.objects.get(name=instance.request.doctor)
+    doctor.available += 1
+    doctor.save()
