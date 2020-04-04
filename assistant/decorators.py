0cb83bd480b470f99fab1489828d782d445dd93c
Saidur Rahman Riad <reidvai1998@gmail.com>
2020-04-04 14:07:39 +0600

    removed: unwanted print statement

--- assistant/decorators.py
+++ assistant/decorators.py
@@ -6,7 +6,6 @@ def assistant_only(view_func):
     if request.user.groups.exists():
       group = request.user.groups.all()[0].name
     if group == 'patient':
-      print(group)
       return redirect('home')
     else:
       return view_func(request, *args, **kwargs)
