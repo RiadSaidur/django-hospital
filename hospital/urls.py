38dd58090075756ed86202f7da7807ff449030bd
RiadSaidu <reidvai1998@gmail.com>
2021-10-17 13:16:45 +0600

    updated: admin site title

--- hospital/urls.py
+++ hospital/urls.py
@@ -10,3 +10,5 @@ urlpatterns = [
     path('assistant/', include('assistant.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+admin.site.site_header = 'Hospital Administration'
