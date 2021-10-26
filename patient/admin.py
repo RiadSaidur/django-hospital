4bee1563e1c9d27646ca948c224a1d2e9b6f770b
RiadSaidu <reidvai1998@gmail.com>
2021-10-26 11:11:20 +0600

    replaced: age to currentState

--- patient/admin.py
+++ patient/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Patient, Request, Appointment
 
 class PatientAdmin(admin.ModelAdmin):
-  fields = ['user', 'fullname', 'dob', 'age', 'gender', 'picture']
+  fields = ['user', 'fullname', 'dob', 'currentState', 'gender', 'picture']
 
 class RequestAdmin(admin.ModelAdmin):
   fields = ['confirmed', 'patient', 'doctor', 'currentState']
